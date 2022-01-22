4980930f287e12c929aa3c5d4fb647f8d56a9651
hydrostaticcog <hydro@hydrostaticcog.me>
2022-01-22 16:16:10 -0600

    3.1.1/refactor embeds again

--- utils/embeds.py
+++ utils/embeds.py
@@ -8,13 +8,10 @@ from utils.ctx_class import MyContext
 async def leaderboard_embed(ctx: MyContext = None, bot: dOGbot = None, board: List = None):
     embed = discord.Embed(title=f"{ctx.guild.name}'s Leaderboard", description="Top 10 users who have the most "
                                                                                "cookies", color=bot.color)
-    counter = 0
-    while counter <= 10:
-        try:
-            embed.add_field(name=str(counter + 1), value=f"{board[counter][0]} - Level {board[counter][1]} "
-                            f"- {board[counter][2]} Cookies",
-                            inline=False)
-        except IndexError:
-            return embed
-        counter += 1
+    for (idx, item) in enumerate(board):
+        if idx == 10:
+            break
+        embed.add_field(name=str(idx + 1), value=f"{item[0]} - Level {item[1]} "
+                                                 f"- {item[2]} Cookies",
+                        inline=False)
     return embed
